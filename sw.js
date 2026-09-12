56a528ae4146e2874f414b8692fc09ad226b0a0c
F23-blip <effeventitre23@gmail.com>
2026-09-12 18:11:54 +0200

    Add files via upload
    index

--- sw.js
+++ sw.js
@@ -11,7 +11,7 @@
 // installata, anche disinstallando e reinstallando l'icona (l'icona è
 // solo una scorciatoia, la cache resta legata al sito, non a quella).
 
-const CACHE_NAME = 'portafoglio-tr-v7';
+const CACHE_NAME = 'portafoglio-tr-v8';
 const FILE_DA_CACHARE = [
   './',
   './index.html',
